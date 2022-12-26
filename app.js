66c2cefddf0ae19e933238dca8885c590ca8fd30
Kweinkauff29 <79805109+Kweinkauff29@users.noreply.github.com>
2022-12-26 18:06:47 -0500

    Updated the homepage to work for returning users

--- app.js
+++ app.js
@@ -51,10 +51,10 @@ var newUser = mongoose.model('excersiseapp', new Schema({
 }, { collection: "test" }, { versionKey: false } ));
 
 app.get("/", async function (req, res) {
-  console.log(req.body);
-  console.log(res.body);
+  const message = "";
+  const noUserMessage = "";
 
-  res.render('home.ejs', {message});
+  res.render('home.ejs', {message: message, noUserMessage: noUserMessage});
 });
 
 
@@ -65,16 +65,16 @@ const server = http.createServer(app);
 const message = "";
 
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   const message = "";
-  console.log(this);
-  res.render('home.ejs', {message});
+  const noUserMessage = "";
+
+  res.render('home.ejs', {message: message, noUserMessage: noUserMessage});
 
 });
 
 app.get('/main/:name', async (req, res) => {
   var name = req.params.name;
-  console.log(name);
   //console.log(collection);
 
   let test = newUser({
@@ -83,8 +83,6 @@ app.get('/main/:name', async (req, res) => {
     email: req.body.email2,
     weight: req.body.weight2
   })
-
-  console.log(req);
 
   const options = { wtimeout: 10000 };
 
@@ -115,13 +113,12 @@ app.get('/main/:name', async (req, res) => {
 app.post('/', async function(req, res) {
   const collection = client.db("excersiseapp").collection("excersiseapp");
   const message = "";
-  console.log(req, "<= req");
+  const noUserMessage = "";
 });
 
-//route that will handle returning users
+//route that will handle New Users
 app.post('/main/:name', async function(req, res) {
   var name = req.params.name;
-  console.log(name);
   const collection = client.db("excersiseapp").collection("excersiseapp1");
 
   //console.log(collection);
@@ -134,16 +131,15 @@ app.post('/main/:name', async function(req, res) {
   })
 
 
-  console.log(req);
-
   const options = { wtimeout: 10000 };
+
+  //check for login
 
 
   //check to see if user already Exists
   const userCheck = await newUser.find( { username: req.body.name2 } );
   console.log(userCheck, '<=usercheck')
 
-  console.log(userCheck);
     if (userCheck.length > 0) {
         console.log("User Already Exists");
         const message = "Username Already Exist.";
@@ -160,6 +156,59 @@ app.post('/main/:name', async function(req, res) {
           });
       }
 
+});
+
+//route that will handle returning users
+app.post('/User-Returning/:name', async function(req, res) {
+  const collection = client.db("excersiseapp").collection("test");
+
+  //hold the username and password from the form
+  let name = req.body.name;
+  let password = req.body.password;
+
+  console.log(name);
+  console.log(password);
+
+  const message = "";
+  const nameToCheck = req.body.name;
+  const passToCheck = req.body.password;
+
+  const userCheck = await collection.findOne( { username: name } );
+
+  //hold records of both username and password from the database
+  const userNameCheck = userCheck.username;
+  const userPass = userCheck.password;
+
+  console.log(userNameCheck, "< username check");
+  console.log(userPass, "< password check");
+
+
+  if (userNameCheck == name) {
+    if (userPass == password) {
+      const noUserMessage = "";
+      console.log("User Already Exists 145");
+      const message = "";
+
+      //Only for right now - Must implement test checker to see where they need to go
+      res.redirect('/User-Returning/' + name);
+      }
+      else {
+        const noUserMessage = "Username or password is incorrect";
+        const message = '';
+        res.render('home.ejs', {noUserMessage: noUserMessage, message: message});
+      }
+    }
+
+    else {
+      const noUserMessage = "Username or password is incorrect";
+      const message = '';
+      res.render('home.ejs', {noUserMessage: noUserMessage, message: message});
+    }
+});
+
+app.get("/User-Returning/:name", async (req, res) => {
+  let name = req.params.name;
+  res.render('main', { name });
 });
 
 
